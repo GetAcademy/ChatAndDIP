1a8690c8e7caa8b2b8f1e66f1a9f902e6bdf5d93
GET Academy <terje@kolderup.net>
2024-06-13 08:53:34 +0200

    Initial commit

---
+++ CountLines/Program.cs
@@ -0,0 +1,17 @@
+﻿using var reader = new StreamReader("file.txt");
+
+var matchingLineCount = 0;
+var totalLineCount = 0;
+var line = reader.ReadLine();
+while (line != null)
+{
+    if (line.Contains("e"))
+    {
+        matchingLineCount++;
+    }
+
+    totalLineCount++;
+    line = reader.ReadLine();
+}
+
+Console.WriteLine($"{matchingLineCount} av {totalLineCount} inneholder e");
